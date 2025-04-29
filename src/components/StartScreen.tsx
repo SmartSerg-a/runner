2bf612b59feaca4037bc472bb2314d52c2ca79ad
SmartSerg-a <136840320+SmartSerg-a@users.noreply.github.com>
2025-04-29 16:27:51 -0400

    Update StartScreen.tsx

--- src/components/StartScreen.tsx
+++ src/components/StartScreen.tsx
@@ -36,14 +36,14 @@ const StartScreen: React.FC = () => {
           Start Game
         </button>
         
-        {/* Кнопка X с SVG */}
+        {/* Обновлённая кнопка X с SVG */}
         <a
-          href="https://x.com/your_username"
+          href="https://x.com/whatphotofuck"
           target="_blank"
           rel="noopener noreferrer"
-          className="absolute top-2 right-2"
+          className="absolute top-4 right-4"
         >
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg width="32" height="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" fill="white" />
           </svg>
         </a>
